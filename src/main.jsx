721a41f8f7e3ace7cce1dbde92e25f9a19a061b1
rishavtiwari22 <rishav@navgurukul.org>
2025-02-10 18:53:30 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 // import LogIn from './components/login'
-import App from './components/App'
+import Html from './components/Html'
 
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Html />
   </StrictMode>,
 )
