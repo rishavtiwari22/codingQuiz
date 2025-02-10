6dd869e6d50ec2822df0a39c414c95776b748696
rishavtiwari22 <rishav@navgurukul.org>
2025-02-10 18:51:20 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import LogIn from './components/login'
+import Main from './components/Main'
 
 
 
